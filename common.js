d534af39e8987f487cd247e9613fcbfc9b91abc8
vramesh4tech <vramesh4tech@gmail.com>
2023-11-20 23:04:15 +0530

    code

---
+++ common.js
@@ -0,0 +1,6 @@
+document.getElementById('submitButton').addEventListener('click', function() {
+    var inputText = document.getElementById('inputBox').value;
+    console.log("Input value: " + inputText);
+    alert("Input value: " + inputText); // Display value in an alert
+  });
+  
